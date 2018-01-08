695640716e1e96ea1c3148bf6b9629b17dcd6592
Naeem Shaikh <naeemshaikh90@yahoo.co.in>
2018-01-08 17:53:37 +0530

    Add MapController

---
+++ TouristHelper/Controllers/MapController.swift
@@ -0,0 +1,18 @@
+//
+//  MapController.swift
+//  TouristHelper
+//
+//  Created by Naeem Shaikh on 08/01/18.
+//  Copyright © 2018 Naeem Shaikh. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
