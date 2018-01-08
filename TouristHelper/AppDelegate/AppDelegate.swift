83fd2b114b3cba75a933b3454c2b033dd7cba969
Naeem Shaikh <naeemshaikh90@yahoo.co.in>
2018-01-08 17:49:50 +0530

    Project structure

---
+++ TouristHelper/AppDelegate/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TouristHelper
+//
+//  Created by Naeem Shaikh on 08/01/18.
+//  Copyright © 2018 Naeem Shaikh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+  
+}
+
