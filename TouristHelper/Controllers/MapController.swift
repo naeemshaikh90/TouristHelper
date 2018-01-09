9f224a805f7f9f254620af6797bd71b032ec29bc
Naeem Shaikh <naeemshaikh90@yahoo.co.in>
2018-01-09 12:37:53 +0530

    Map focus on user location

--- TouristHelper/Controllers/MapController.swift
+++ TouristHelper/Controllers/MapController.swift
@@ -44,12 +44,18 @@ extension MapController: CLLocationManagerDelegate {
       print("Current Location: \(location.latitude), \(location.longitude)")
       currentLocation = location
       locationManager.stopUpdatingLocation()
+      setMapFocusOnUserLocation()
     }
   }
 }
 
 // MARK: - GMSMapViewDelegate
 extension MapController: GMSMapViewDelegate {
-  func initMap() {
+  func setMapFocusOnUserLocation() {
+    let latitude = currentLocation.latitude
+    let longitude = currentLocation.longitude
+    let camera = GMSCameraPosition.camera(withLatitude: Double(latitude), longitude: Double(longitude), zoom: 10)
+    mapView.camera = camera
+    mapView.isMyLocationEnabled = true
   }
 }
